a8b192483dd321ba0576ad4d12fbe4f29492e184
Charlie Welsh <charlie@allotrope.xyz>
2021-01-18 00:30:09 +0000

    Added color

--- imgGen.cpp
+++ imgGen.cpp
@@ -14,17 +14,23 @@
 #include <fstream>
 #include <string>
 
-const int HEIGHT = 250;
-const int WIDTH = 300;
+const int HEIGHT = 500;
+const int WIDTH = 512;
+
+struct Color {
+	unsigned char red;
+	unsigned char blue;
+	unsigned char green;
+};
 
 /**
 *	Creates the image.
 *
-*	@param	image - the 2D array to plot grayscale colors on
-*				height - the height of the array
+*	@param	image - the 2D array of Color structs to plot colors on
+*			height - the height of the array
 */
 
-void createImage(unsigned char image[][WIDTH], int height);
+void createImage(Color image[][WIDTH], int height);
 
 /**
 *	Writes the 2D array to disk as a PGM file.
@@ -36,7 +42,7 @@ void createImage(unsigned char image[][WIDTH], int height);
 *		 false otherwise
 */
 
-bool writeImage(const unsigned char image[][WIDTH], int height, const std::string fileName);
+bool writeImage(const Color image[][WIDTH], int height, const std::string fileName);
 
 /**
 *	Draws a rectangle to the 2D array.
@@ -47,10 +53,10 @@ bool writeImage(const unsigned char image[][WIDTH], int height, const std::strin
 *			rectLeft - the left bound of the rectangle
 *			rectHeight - the height of the rectangle
 *			rectWidth - the width of the rectangle
-*			grayLevel - the intensity of gray to draw with
+*			color - the color to draw with
 */
 
-void drawRect(unsigned char image[][WIDTH], int imgHeight, int rectTop, int rectLeft, int rectHeight, int rectWidth, unsigned char grayLevel);
+void drawRect(Color image[][WIDTH], int imgHeight, int rectTop, int rectLeft, int rectHeight, int rectWidth, Color color);
 
 /**
 *	Draws a circle to the 2D array.
@@ -60,68 +66,72 @@ void drawRect(unsigned char image[][WIDTH], int imgHeight, int rectTop, int rect
 *			centerX - the x-coordinate of the center of the circle
 *			centerY - the y-coordinate of the center of the circle
 *			radius - the radius of the circle
-*			grayLevel - the intensity of gray to draw with
+*			color - the color to draw with
 */
 
-void drawCircle(unsigned char image[][WIDTH], int height, int centerX, int centerY, int radius, unsigned char grayLevel);
+void drawCircle(Color image[][WIDTH], int height, int centerX, int centerY, int radius, Color color);
 
 
 int main()
 {
 	// set array to white
-	unsigned char image[HEIGHT][WIDTH];
+	Color image[HEIGHT][WIDTH];
 	createImage(image, HEIGHT);
-	writeImage(image, HEIGHT, "foo.pgm");
+	writeImage(image, HEIGHT, "foo.ppm");
 }
 
 // ------------------------------------------------------
 
-void createImage(unsigned char image[][WIDTH], int height) {
+void createImage(Color image[][WIDTH], int height) {
 	// Initializes the image to a blank white background
 	for (int row = 0; row < WIDTH; row++) {
 		for (int column = 0; column < height; column++) {
-			image[column][row] = 255;
+			image[column][row].red = 255;
+			image[column][row].green = 255;
+			image[column][row].blue = 255;
 		}
 	}
 	
-	drawRect(image, height, 120, 20, 40, 20, 20);
-	drawRect(image, height, 223, 65, 24, 50, 20);
-	drawCircle(image, height, 50, 50, 31, 0);
-	drawCircle(image, height, 72, 143, 42, 0);
+	Color red = { .red = 163, .green = 66, .blue = 58 };
+	
+	//drawRect(image, height, 120, 20, 40, 20, red);
+	//drawRect(image, height, 223, 65, 24, 50, red);
+	drawCircle(image, height, 256, 250, 89, red);
+	//drawCircle(image, height, 72, 143, 42, red);
 }
 
 // ------------------------------------------------------
 
-void drawRect(unsigned char image[][WIDTH], int imgHeight, int rectTop, int rectLeft, int rectHeight, int rectWidth, unsigned char grayLevel) {
+void drawRect(Color image[][WIDTH], int imgHeight, int rectTop, int rectLeft, int rectHeight, int rectWidth, Color color) {
 	// Draws the top and bottom line of the rectangle
 	for (int horizontalPosition = rectLeft; horizontalPosition < rectLeft + rectWidth; horizontalPosition++) {
-		image[rectTop][horizontalPosition] = grayLevel;
-		image[rectTop + rectHeight - 1][horizontalPosition] = grayLevel;
+		image[rectTop][horizontalPosition] = color;
+		image[rectTop + rectHeight - 1][horizontalPosition] = color;
 	}
 	// Draws the left and right lines of the rectangle
 	for (int verticalPosition = rectTop; verticalPosition < rectTop + rectHeight; verticalPosition++) {
-		image[verticalPosition][rectLeft] = grayLevel;
-		image[verticalPosition][rectLeft + rectWidth - 1] = grayLevel;
+		image[verticalPosition][rectLeft] = color;
+		image[verticalPosition][rectLeft + rectWidth - 1] = color;
 	}
 }
 
 // ------------------------------------------------------
 
-void drawCircle(unsigned char image[][WIDTH], int height, int centerX, int centerY, int radius, unsigned char grayLevel) {
+void drawCircle(Color image[][WIDTH], int height, int centerX, int centerY, int radius, Color color) {
 	int x = radius;
 	int y = 0;
 	int err = 0;
  
 	while (x >= y) {
 		// Fill in all 4 quadrants at once.
-		image[centerY + y][centerX + x] = grayLevel;
-		image[centerY + x][centerX + y] = grayLevel;
-		image[centerY + x][centerX - y] = grayLevel;
-		image[centerY + y][centerX - x] = grayLevel;
-		image[centerY - y][centerX - x] = grayLevel;
-		image[centerY - x][centerX - y] = grayLevel;
-		image[centerY - x][centerX + y] = grayLevel;
-		image[centerY - y][centerX + x] = grayLevel;
+		image[centerY + y][centerX + x] = color;
+		image[centerY + x][centerX + y] = color;
+		image[centerY + x][centerX - y] = color;
+		image[centerY + y][centerX - x] = color;
+		image[centerY - y][centerX - x] = color;
+		image[centerY - x][centerX - y] = color;
+		image[centerY - x][centerX + y] = color;
+		image[centerY - y][centerX + x] = color;
 	
 		if (err <= 0) {
 			y += 1;
@@ -137,15 +147,15 @@ void drawCircle(unsigned char image[][WIDTH], int height, int centerX, int cente
 
 // ------------------------------------------------------
 
-bool writeImage(const unsigned char image[][WIDTH], int height, const std::string fileName) {
+bool writeImage(const Color image[][WIDTH], int height, const std::string fileName) {
 	std::ofstream myfile;
 	myfile.open (fileName);
 	
-	myfile << "P2" << std::endl << "# " << fileName << std::endl << std::endl << WIDTH << " " << height << std::endl << 256 << std::endl;
+	myfile << "P3" << std::endl << "# " << fileName << std::endl << std::endl << WIDTH << " " << height << std::endl << 255 << std::endl;
 	
 	for (int row = 0; row < height; row++) {
 		for (int column = 0; column < WIDTH; column++) {
-			myfile << static_cast<int>(image[row][column]) << " ";
+			myfile << static_cast<int>(image[row][column].red) << " " << static_cast<int>(image[row][column].green) << " " << static_cast<int>(image[row][column].blue) << " ";
 		}
 		myfile << std::endl;
 	}
